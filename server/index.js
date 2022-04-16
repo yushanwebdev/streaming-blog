9b3a6a8a243b9a68d246d0f0f9191c0935cbda88
yushan <yushanwebdev@gmail.com>
2022-04-16 21:10:26 +0530

    feat: Develop an offline template and make that a static asset

--- server/index.js
+++ server/index.js
@@ -1,37 +1,50 @@
-const express = require('express');
-const del = require('del');
-const nunjucks = require('nunjucks');
+const express = require("express");
+const del = require("del");
+const nunjucks = require("nunjucks");
 const app = express();
 
-const rev = require('./rev');
+const rev = require("./rev");
 
-app.set('view engine', 'njk');
+app.set("view engine", "njk");
 
-const nunjucksEnv = nunjucks.configure(__dirname + '/../templates', {
+const nunjucksEnv = nunjucks.configure(__dirname + "/../templates", {
   watch: true,
-  express: app
+  express: app,
 });
 
-require('nunjucks-date-filter').install(nunjucksEnv);
-nunjucksEnv.addFilter('rev', str => rev.get(str));
+require("nunjucks-date-filter").install(nunjucksEnv);
+nunjucksEnv.addFilter("rev", (str) => rev.get(str));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // Rev static files
-(async function() {
+(async function () {
   const staticRevPath = `${__dirname}/../static-rev`;
   await del(staticRevPath);
-  await rev.copyAndRev(`${__dirname}/../static`, '**', staticRevPath);
-  await rev.addAndRev('offline-inc.html', staticRevPath, nunjucksEnv.render('offline-inc.njk'));
+  await rev.copyAndRev(`${__dirname}/../static`, "**", staticRevPath);
 
-  const shell = nunjucksEnv.render('shell.njk');
-  const splitAt = '<!-- content go here -->';
+  const shell = nunjucksEnv.render("shell.njk");
+  const splitAt = "<!-- content go here -->";
 
-  await rev.addAndRev('shell-start.html', staticRevPath, shell.slice(0, shell.indexOf(splitAt)));
-  await rev.addAndRev('shell-end.html', staticRevPath, shell.slice(shell.indexOf(splitAt) + splitAt.length));
+  await rev.addAndRev(
+    "shell-start.html",
+    staticRevPath,
+    shell.slice(0, shell.indexOf(splitAt))
+  );
+  await rev.addAndRev(
+    "shell-end.html",
+    staticRevPath,
+    shell.slice(shell.indexOf(splitAt) + splitAt.length)
+  );
   await rev.replaceInFiles(`${__dirname}/../static-rev/**/*.css`);
 
+  await rev.addAndRev(
+    "offline.html",
+    staticRevPath,
+    nunjucks.render("offline.njk")
+  );
+
   app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log("Example app listening on port 3000!");
   });
 })();
